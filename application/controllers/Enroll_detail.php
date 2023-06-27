87afa20a6d695ec23a200d269e91393f30f9903a
Yongyuttt <pornpongsawadi.ko@gmail.com>
2023-06-27 13:42:12 +0700

    add enroll_detail

---
+++ application/controllers/Enroll_detail.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Enroll_detail extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	/** ------------------------------------------------------- **
+	 **           MANDATORY SCRIPT
+    /** ------------------------------------------------------- **/
+	public function index()
+	{
+		$this->load->view('header');
+		$this->load->view('enroll_detail');
+		$this->load->view('footer');
+	}
+}
